552f99374dbe782a2933e2901a75c3bd2bfbdba0
Gaël <gael+git@demette.fr>
2018-11-30 16:12:54 +0100

    Insert map

---
+++ components/map.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="map-wrap">
+    <no-ssr>
+      <l-map
+        :zoom="13"
+        :center="[47.413220, -1.219482]"
+      >
+        <l-tile-layer url="//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <l-marker :lat-lng="[47.413220, -1.219482]" />
+      </l-map>
+    </no-ssr>
+  </div>
+</template>
