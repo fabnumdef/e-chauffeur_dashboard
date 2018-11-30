5a6b27956d79b6be08d8cb2a99b99f09b0664560
Gaël <gael+git@demette.fr>
2018-11-30 13:07:23 +0100

    Install nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,89 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'spa',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' },
+    ]
+  },
+
+  router: {
+    middleware: ['auth'],
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    { src: '~assets/css/main.scss', lang: 'scss' }
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~/api',
+    '~/plugins/multiselect.js',
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    'nuxt-leaflet',
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios',
+    '@nuxtjs/toast',
+    '@nuxtjs/auth',
+    ['qonfucius-nuxt-bulma', { css: false, postcss: false }],
+    'qonfucius-nuxt-fontawesome',
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  auth: {
+    redirect: {
+      login: '/login',
+      logout: '/',
+      callback: '/login',
+      home: '/'
+    },
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: '/jwt/generate', method: 'post', propertyName: 'token' },
+          user: { url: '/jwt/user?mask=id,email', method: 'get', propertyName: false },
+        },
+      },
+    },
+  },
+
+  fontAwesome: {
+    packs: [
+      {
+        package: '@fortawesome/free-solid-svg-icons',
+        icons: ['faMapMarkerAlt', 'faFlag', 'faEdit', 'faTrash', 'faPlus', 'faSave'],
+      },
+    ],
+  },
+};
