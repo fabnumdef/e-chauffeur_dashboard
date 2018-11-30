ef7252200798d3cda320f25e639cd960723fcf61
Gaël <gael+git@demette.fr>
2018-11-30 16:12:41 +0100

    GPS point form component

---
+++ components/form/gps-point.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="field has-addons">
+      <p class="control">
+        <input
+          ref="longitude"
+          :value="value.coordinates[0]"
+          type="number"
+          placeholder="Longitude"
+          class="input"
+          @input="input">
+      </p>
+      <p class="control">
+        <input
+          ref="latitude"
+          :value="value.coordinates[1]"
+          type="number"
+          placeholder="Latitude"
+          class="input"
+          @input="input">
+      </p>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Object,
+      default: () => ({
+        coordinates: [null, null],
+      }),
+    },
+  },
+  methods: {
+    input() {
+      this.$emit(
+        'input',
+        {
+          type: 'Point',
+          coordinates: ['longitude', 'latitude'].map(
+            k => this.$refs[k].value || null,
+          ),
+        },
+      );
+    },
+  },
+};
+</script>
