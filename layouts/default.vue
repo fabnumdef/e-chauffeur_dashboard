43513fa7c532f9874b9e5c585f5f58af3da16747
Gaël <gael+git@demette.fr>
2018-11-30 15:45:37 +0100

    Create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="columns">
+    <ec-sidemenu class="column is-narrow is-menu" />
+    <nuxt class="column" />
+    <ec-map class="column is-map" />
+  </div>
+</template>
+<script>
+import ecSidemenu from '~/components/sidemenu.vue';
+import ecMap from '~/components/map.vue';
+
+export default {
+  components: {
+    ecSidemenu,
+    ecMap,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import "~assets/css/head";
+
+  .is-menu {
+    padding-right: 0;
+    background: $blue-dark;
+    margin-bottom: -$size-small;
+  }
+  .columns {
+    height: 100%;
+  }
+  .is-map {
+    margin-bottom: -$size-small * 2;
+  }
+</style>
