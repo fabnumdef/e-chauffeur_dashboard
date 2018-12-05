3a6409ea7283869c1544bb2df7154d61b5284f48
Gaël <gael+git@demette.fr>
2018-12-05 10:51:25 +0100

    Create field to search car model

---
+++ components/form/search-car-models.vue
@@ -0,0 +1,44 @@
+<template>
+  <vue-multiselect
+    :placeholder="placeholder"
+    :id="id"
+    :options="carModels"
+    :value="value"
+    track-by="id"
+    label="label"
+    @search-change="updateSet"
+    @input="onInput"
+  />
+</template>
+<script>
+import debounce from 'lodash.debounce';
+
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: null,
+    },
+    id: {
+      type: String,
+      default: '',
+    },
+    value: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data: () => ({
+    carModels: [],
+  }),
+  methods: {
+    updateSet: debounce(async function updateSet(search) {
+      const { data } = await this.$api.carModels.getCarModels('id,label', { search });
+      this.carModels = data;
+    }, 500),
+    onInput(data) {
+      this.$emit('input', data);
+    },
+  },
+};
+</script>
