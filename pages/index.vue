5bd4970b1f12ee955d2145bf756cb075e0132e25
Gaël <gael+git@demette.fr>
2019-01-31 17:21:00 +0100

    Accueil

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <section>
-    Index
-  </section>
+  <main>
+    <h1 class="title">
+      Accueil
+    </h1>
+    <p>Bienvenue sur le portail e-chauffeur, merci de choisir la base à visualiser via le menu à gauche.</p>
+  </main>
 </template>
