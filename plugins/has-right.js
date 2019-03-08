0171255c8127f8b4eb85d2d6b625ed1e7e7442e1
Gaël <gael+git@demette.fr>
2019-03-08 11:40:20 +0100

    Extends $auth with hasRight

---
+++ plugins/has-right.js
@@ -0,0 +1,12 @@
+export default function ({ app }) {
+  Object.assign(app.$auth, {
+    hasRight(right, campus) {
+      if (!this.user || !this.user.cachedRights) {
+        return false;
+      }
+      return this.user.cachedRights.reduce((acc, { rights, campuses }) => {
+        return acc || (rights.includes(right) && (!campus || campuses.includes(campus)));
+      }, false);
+    },
+  });
+}
