9bdda07aa89833c6d05ec8f3cc2f4079239432b8
Rodrive <robin.lebert@gmail.com>
2019-03-15 14:45:57 +0100

    Fix click on calendar icon

--- pages/_campus/rides/index.vue
+++ pages/_campus/rides/index.vue
@@ -15,6 +15,12 @@
           <fa-icon icon="calendar-alt" />
         </template>
       </date-time>
+      <button
+        class="button is-primary"
+        @click="toToday"
+      >
+        Aujourd'hui
+      </button>
     </div>
     <vue-calendar
       :events="rides"
@@ -466,6 +472,9 @@ export default {
     can: ({ status }, action) => {
       const state = new Status(status);
       return state.can(action);
+    },
+    toToday() {
+      this.today = new Date();
     },
   },
 };
