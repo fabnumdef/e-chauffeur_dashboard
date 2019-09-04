60efb95201820f3bece68f0b7023c93528d6b5df
Gaël <gael+git@demette.fr>
2019-09-04 17:00:53 +0200

    Add some styling for this calendar

--- pages/_campus/planning/drivers.vue
+++ pages/_campus/planning/drivers.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="box">
-      <div class="title">Planning</div>
+      <div class="title">
+        Planning
+      </div>
       <client-only>
         <ec-modal
           :active="isModalOpen"
@@ -49,10 +51,21 @@
               :show-labels="false"
             >
               <template #option="{option}">
-                {{ option }}{{ option.firstname }} {{ option.lastname }}
+                {{ option.firstname }} {{ option.lastname }}
               </template>
-              <template #tag="{option}">
-                {{ option }}{{ option.firstname }} {{ option.lastname }}
+              <template #tag="{option, search, remove}">
+                <span class="multiselect__tag">
+                  <span v-if="option.firstname || option.lastname">{{ option.firstname }} {{ option.lastname }}</span>
+                  <span v-else>{{ option.id }}</span>
+                  <i
+                    aria-hidden="true"
+                    tabindex="1"
+                    class="multiselect__tag-icon"
+                    @keypress.enter.prevent="remove(option)"
+                    @mousedown.prevent="remove(option)"
+                  />
+                </span>
+                <span class="multiselect-tag" />
               </template>
             </vue-multiselect>
           </ec-field>
@@ -90,17 +103,27 @@
       <main class="columns">
         <aside class="column is-one-quarter">
           <button
-            class="button"
+            class="button create-timeslot is-expanded"
             @click="openCreate()"
           >
-            Configurer un nouveau créneau
+            <fa-icon icon="user-circle" /> Configurer un nouveau créneau
           </button>
+          <div class="drivers-expander">
+            <span>Chauffeurs</span>
+          </div>
           <ul>
             <li
               v-for="driver of drivers.data"
               :key="driver.id"
+              class="box driver-box"
             >
-              {{ driver.firstname }} {{driver.lastname}}
+              {{ driver.firstname }} {{ driver.lastname }}
+              <nuxt-link
+                class="config-button is-pulled-right"
+                :to="campusLink('drivers-id-edit', {params:{id: driver.id}})"
+              >
+                <fa-icon icon="cog" />
+              </nuxt-link>
             </li>
           </ul>
         </aside>
@@ -117,14 +140,21 @@
             @event-focus="openEdit"
           >
             <template #event-renderer="{ event: {content}, view }">
-              {{ content.start.toLocaleString(TIME_SIMPLE) }}
-              {{ content.end.toLocaleString(TIME_SIMPLE) }}
-              <ul>
+              <header>
+                <span class="hours">
+                  {{ content.start.toLocaleString(TIME_SIMPLE) }} - {{ content.end.toLocaleString(TIME_SIMPLE) }}
+                </span>
+                <span class="is-pulled-right">
+                  <fa-icon icon="user-circle" /> {{ content.drivers.length }}({{ drivers.data.length }})
+                </span>
+              </header>
+              <ul class="drivers-list">
                 <li
                   v-for="driver of content.drivers"
                   :key="driver.id"
                 >
-                  {{ driver.id }}
+                  <span v-if="driver.firstname || driver.lastname">{{ driver.firstname }} {{ driver.lastname }}</span>
+                  <span v-else>{{ driver.id }}</span>
                 </li>
               </ul>
             </template>
@@ -144,7 +174,7 @@ import ecModal from '~/components/modal.vue';
 import ecField from '~/components/form/field.vue';
 
 const DRIVER_DATA = 'id,firstname,lastname';
-const TIMESLOT_DATA = 'id,start,end,drivers(id,firstname,lastname)';
+const TIMESLOT_DATA = `id,start,end,drivers(${DRIVER_DATA})`;
 const newTimeSlot = () => ({
   start: null,
   end: null,
@@ -252,7 +282,73 @@ export default {
 </script>
 <style scoped lang="scss">
   @import "~assets/css/head";
-  .box .title {
-    color: $black;
+  .box {
+    .title {
+      color: $black;
+    }
+    &.driver-box {
+      border-radius: $gap;
+      padding: $size-small;
+      font-weight: bold;
+      .config-button {
+        color: $dark-gray;
+        &:hover {
+          color: $gray;
+        }
+      }
+    }
+  }
+  .create-timeslot {
+    border-radius: $gap;
+    padding: $size-small;
+    width: 100%;
+    position: relative;
+    /deep/ .fa-user-circle {
+      position: absolute;
+      left: 4px;
+      color: $white;
+      background: $success;
+      padding: 5px;
+      box-sizing: initial;
+      border-radius: $gap;
+    }
+  }
+  .drivers-expander {
+    position: relative;
+    margin: $size-small;
+    span {
+      color: $dark-gray;
+      background: $white;
+      font-weight: bold;
+      z-index: 1;
+      position: relative;
+      padding-right: 4px;
+      font-size: $size-6;
+    }
+    &:before {
+      background: 1px $dark-gray;
+      height: 1px;
+      width: 100%;
+      content: " ";
+      display: block;
+      top: $size-6;
+      right: 0;
+      position: absolute;
+      z-index: 0;
+    }
+  }
+  /deep/ .vuecal__event {
+    background: $success;
+    color: $white;
+    padding: 8px;
+    header {
+      text-align: left;
+      .hours {
+        font-weight: bold;
+      }
+    }
+    .drivers-list {
+      font-weight: bold;
+    }
   }
 </style>
