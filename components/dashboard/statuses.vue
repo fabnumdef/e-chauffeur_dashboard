718a128ea4f49c25e0196e599f56f7655abb4f67
Charles Capelli <charles.capelli@qonfucius.team>
2019-12-10 13:33:56 +0100

    Added translation to status

--- components/dashboard/statuses.vue
+++ components/dashboard/statuses.vue
@@ -16,7 +16,7 @@
           <span>Détail</span>
           <ul>
             <li
-              v-for="{total, id} of statuses.slice(0, 3)"
+              v-for="{total, id} of translatedStatuses.slice(0, 3)"
               :key="id"
             >
               {{ id }} (<strong>{{ total }}</strong> course(s))
@@ -26,7 +26,7 @@
       </ul>
     </template>
     <template
-      v-if="statuses.length"
+      v-if="translatedStatuses.length"
       #PIE
     >
       <pie-chart :chart-data="pieStatuses" />
@@ -80,13 +80,75 @@ export default {
     ridesFailed: makeRidesComputedFrom(FAILED_STATUSES),
     ridesCanceled: makeRidesComputedFrom(CANCELED_STATUSES),
     ridesInProgress: makeRidesComputedFrom(IN_PROGRESS_STATUSES),
+    translatedStatuses() {
+      return this.statuses.map((status) => {
+        let id;
+        switch (status.id) {
+          case 'drafted':
+            id = 'En création';
+            break;
+          case 'created':
+            id = 'Créée';
+            break;
+          case 'validated':
+            id = 'Validée';
+            break;
+          case 'rejected_boundary':
+            id = 'Refusée - Hors-limite';
+            break;
+          case 'rejected_capacity':
+            id = 'Refusée - Capacité';
+            break;
+          case 'accepted':
+            id = 'Acceptée par le chauffeur';
+            break;
+          case 'declined':
+            id = 'Rejetée par le chauffeur';
+            break;
+          case 'started':
+            id = 'Commencée';
+            break;
+          case 'waiting':
+            id = 'En attente';
+            break;
+          case 'progress':
+            id = 'En cours';
+            break;
+          case 'delivered':
+            id = 'Terminée';
+            break;
+          case 'canceled':
+            id = 'Annulée';
+            break;
+          case 'canceled_technical':
+            id = 'Annulée - raisons techniques';
+            break;
+          case 'canceled_requested_customer':
+            id = 'Annulée - raisons techniques';
+            break;
+          case 'canceled_customer_overload':
+            id = 'Annulée - passager trop chargé';
+            break;
+          case 'canceled_customer_missing':
+            id = 'Annulée - passager manquant';
+            break;
+          default:
+            id = 'Statut manquant';
+            break;
+        }
+        return {
+          ...status,
+          id,
+        };
+      });
+    },
     ridesTotal() {
       return this.statuses.reduce((acc, { total }) => (acc + total), 0);
     },
     pieStatuses() {
       return {
-        labels: this.statuses.map(({ id }) => id),
-        datasets: [{ ...this.backgroundColors, data: this.statuses.map(({ total }) => total) }],
+        labels: this.translatedStatuses.map(({ id }) => id),
+        datasets: [{ ...this.backgroundColors, data: this.translatedStatuses.map(({ total }) => total) }],
       };
     },
   },
