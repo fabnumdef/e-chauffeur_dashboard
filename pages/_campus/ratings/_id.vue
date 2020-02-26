86665804d3c4997b1f5372e40a0aab342146cd24
Charles Capelli <charles.capelli@qonfucius.team>
2020-02-26 15:31:08 +0100

    Added rating by campus for superadmin only :
    - Rating list
    - Rating details with link to ride details
    - Ride details

---
+++ pages/_campus/ratings/_id.vue
@@ -0,0 +1,62 @@
+<template>
+  <main>
+    <header>
+      <h1
+        class="title"
+      >
+        Appréciation <em class="is-size-6">#{{ id }}</em>
+      </h1>
+    </header>
+    <section
+      class="box"
+    >
+      <ul>
+        <li>Note Ux : <strong>{{ uxGrade }} / 5</strong></li>
+        <li>Recommandation : <strong>{{ recommandationGrade }} / 10</strong></li>
+        <li v-if="message">
+          Message :
+        </li>
+        <li v-if="message">
+          {{ message }}
+        </li>
+        <li>Date : <strong>{{ createdAt }}</strong></li>
+        <li>
+          Id de la course : <strong>
+            <nuxt-link :to="campusLink('rides-id', { params: { id: ride.id } })">{{ ride.id }}</nuxt-link>
+          </strong>
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
+
+<script>
+import { DateTime } from 'luxon';
+
+const mask = 'id,uxGrade,recommandationGrade,message,createdAt,ride';
+
+export default {
+  async asyncData({ params, $api }) {
+    const { data } = await $api.ratings(params.campus, mask).getRating(params.id);
+    const rating = {
+      ...data,
+      createdAt: DateTime.fromISO(data.createdAt).toFormat('dd LLL yyyy à HH:mm', { locale: 'fr' }),
+    };
+    return { ...rating };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '~assets/css/head';
+  h1 {
+    margin-bottom: .6em;
+  }
+  a {
+    color: inherit;
+    text-decoration: underline;
+    &:hover {
+      color: $dark-gray;
+    }
+  }
+</style>
---
+++ pages/_campus/rides/_id.vue
@@ -0,0 +1,83 @@
+<template>
+  <main>
+    <header>
+      <h1
+        class="title"
+      >
+        Course <em class="is-size-6">#{{ id }}</em>
+      </h1>
+    </header>
+    <section
+      class="box"
+    >
+      <ul>
+        <li>Catégorie : <strong>{{ category.label }}</strong></li>
+        <li>Départ : <strong>{{ departure.label }}</strong> le <strong>{{ start }}</strong></li>
+        <li>Arrivée : <strong>{{ arrival.label }}</strong> le <strong>{{ end }}</strong></li>
+        <li>Chauffeur : <strong>{{ driver.firstname }} {{ driver.lastname }}</strong></li>
+        <li>Véhicule : <strong>{{ car.id }}</strong></li>
+        <li>Statut : <strong>{{ convertStatus(status) }}</strong></li>
+        <li>Nombre de passagers : <strong>{{ passengersCount }}</strong></li>
+        <li>Présence de bagages : <strong>{{ luggage ? 'Oui' : 'Non' }}</strong></li>
+        <li v-if="comments">
+          Commentaires : <strong>{{ comments }}</strong>
+        </li>
+        <li>Date de création : <strong>{{ createdAt }}</strong></li>
+      </ul>
+
+    </section>
+  </main>
+</template>
+
+<script>
+import { DateTime } from 'luxon';
+import {
+  ACCEPTED,
+  DELIVERED,
+  IN_PROGRESS,
+  STARTED,
+  VALIDATED,
+  WAITING,
+} from '@fabnumdef/e-chauffeur_lib-vue/api/status/states';
+
+const mask = '*';
+const formatDate = (ISODate) => DateTime.fromISO(ISODate).toFormat('dd LLL yyyy à HH:mm', { locale: 'fr' });
+
+export default {
+  async asyncData({ params, $api }) {
+    const { data } = await $api.rides(params.campus, mask).getRide(params.id);
+    const ride = {
+      ...data,
+      start: formatDate(data.start),
+      end: formatDate(data.end),
+      createdAt: formatDate(data.createdAt),
+    };
+    return {
+      ...ride,
+    };
+  },
+  methods: {
+    convertStatus(status) {
+      switch (status) {
+        case VALIDATED:
+        case ACCEPTED:
+          return 'Planifié';
+        case STARTED:
+        case WAITING:
+        case IN_PROGRESS:
+          return 'En cours';
+        case DELIVERED:
+          return 'Délivré';
+        default:
+          return null;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    margin-bottom: .6em;
+  }
+</style>
