ca5f985bb90986d6ebc6090465d75e22c8a29c58
Gaël Demette <gael.demette+git@qonfucius.team>
2020-04-23 18:23:05 +0200

    Refactor car-models to be compliant with new lib-vue

--- pages/car-models/_id/edit.vue
+++ pages/car-models/_id/edit.vue
@@ -1,106 +1,78 @@
 <template>
-  <main>
-    <header>
-      <h1
-        v-if="id"
-        class="title"
-      >
-        Modèle de véhicule #{{ id }}
-      </h1>
-      <h1
-        v-else
-        class="title"
-      >
-        Modèle de véhicule
-      </h1>
-      <h2
-        v-if="id"
-        class="subtitle"
-      >
-        Modification
-      </h2>
-      <h2
-        v-else
-        class="subtitle"
-      >
-        Création
-      </h2>
-    </header>
-    <form
-      class="box"
-      @submit.prevent="edit(carModel)"
+  <form
+    class="box"
+    @submit.prevent="edit(data, $event)"
+  >
+    <ec-field
+      label="ID"
+      field-id="id"
+      :error-message="getErrorMessage('id')"
     >
-      <ec-field
-        label="ID"
-        field-id="id"
+      <input
+        id="id"
+        v-model.trim="data.id"
+        :disabled="!!id"
+        class="input"
+        :class="getErrorClass('id')"
       >
-        <input
-          id="id"
-          v-model.trim="carModel.id"
-          :disabled="!!id"
-          class="input"
-        >
-      </ec-field>
+    </ec-field>
 
-      <ec-field
-        label="Label"
-        field-id="label"
+    <ec-field
+      label="Label"
+      field-id="label"
+      :error-message="getErrorMessage('label')"
+    >
+      <input
+        id="label"
+        v-model.trim="data.label"
+        class="input"
+        :class="getErrorClass('label')"
       >
-        <input
-          id="label"
-          v-model.trim="carModel.label"
-          class="input"
-        >
-      </ec-field>
+    </ec-field>
 
-      <ec-field
-        label="Capacité du véhicule"
-        field-id="capacity"
+    <ec-field
+      label="Capacité du véhicule"
+      field-id="capacity"
+      :error-message="getErrorMessage('capacity')"
+    >
+      <input
+        id="capacity"
+        v-model.number="data.capacity"
+        class="input"
+        type="number"
+        :class="getErrorClass('capacity')"
       >
-        <input
-          id="capacity"
-          v-model.number="carModel.capacity"
-          class="input"
-          type="number"
-        >
-      </ec-field>
+    </ec-field>
 
-      <button
-        v-if="id"
-        type="submit"
-        class="button is-primary"
-        :class="{'is-loading': loading}"
-        :disabled="loading"
-      >
-        <span class="icon is-small">
-          <fa-icon :icon="['fas', 'save']" />
-        </span>
-        <span>Sauvegarder</span>
-      </button>
-
-      <button
-        v-else
-        type="submit"
-        class="button is-primary"
-        :class="{'is-loading': loading}"
-        :disabled="loading"
-      >
-        <span class="icon is-small">
-          <fa-icon :icon="['fas', 'plus']" />
-        </span>
-        <span>Créer</span>
-      </button>
-    </form>
-  </main>
+    <save-button
+      :loading="loading"
+      :is-new="!id"
+      has-alt
+    />
+  </form>
 </template>
 
 <script>
 import toggleLoading from '~/helpers/mixins/toggle-loading';
-
-const mask = 'id,label,capacity';
+import errorsManagementMixin from '~/helpers/mixins/errors-management';
+import resetableMixin from '~/helpers/mixins/reset-data';
+import titleMixin from '~/helpers/mixins/page-title';
+import saveButton, { NEXT_ACTION_KEY, NEXT_ACTION_LIST, NEXT_ACTION_NEW } from '~/components/crud/save-button.vue';
 
 export default {
-  mixins: [toggleLoading()],
+  components: {
+    saveButton,
+  },
+  mixins: [
+    toggleLoading(),
+    titleMixin('Modèle de véhicule', 'Création'),
+    errorsManagementMixin(),
+    resetableMixin(function reset() {
+      return {
+        data: { ...this.carModel },
+      };
+    }),
+  ],
   props: {
     carModel: {
       type: Object,
@@ -108,28 +80,38 @@ export default {
     },
   },
   data() {
-    return { id: this.carModel.id };
+    const { label, id } = this.carModel;
+    this.setTitle(id ? `Modèle de véhicule #${id}: ${label}` : 'Modèle de véhicule', id ? 'Édition' : 'Création');
+    return { id };
   },
   methods: {
-    async edit(carModel) {
-      let data = {};
-      try {
-        this.toggleLoading(true);
+    async edit(carModel, { submitter }) {
+      return this.raceToggleLoading(() => this.handleCommonErrorsBehavior(async () => {
+        const ApiCarModels = this.$api.query('carModels').setMask('id,label,capacity');
+        let data = {};
         if (this.id) {
-          ({ data } = (await this.$api.carModels.patchCarModel(this.id, carModel, mask)));
+          ({ data } = (await ApiCarModels.edit(carModel.id, carModel)));
         } else {
-          ({ data } = (await this.$api.carModels.postCarModel(carModel, mask)));
+          ({ data } = (await ApiCarModels.create(carModel)));
         }
-
-        this.$toast.success('Donnée enregistrée avec succès');
-        this.$router.push({
-          name: 'car-models-id-edit',
-          params: { id: data.id },
-        });
-      } catch {
-        this.$toast.error('Une erreur est survenue, merci de vérifier les champs.');
-      }
-      this.toggleLoading(false);
+        this.$toast.success('Modèle de véhicule enregistrée avec succès');
+        switch (submitter.getAttribute(NEXT_ACTION_KEY)) {
+          case NEXT_ACTION_NEW:
+            this.reset();
+            return this.$router.push({
+              name: 'car-models-new',
+            });
+          case NEXT_ACTION_LIST:
+            return this.$router.push({
+              name: 'car-models',
+            });
+          default:
+            return this.$router.push({
+              name: 'car-models-id-edit',
+              params: { id: data.id },
+            });
+        }
+      }));
     },
   },
 };
